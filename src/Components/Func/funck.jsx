9c08402d7189a07199af95894448f3ab2306ec54
Abdurazakov xasan <102683894+Xasano5o@users.noreply.github.com>
2022-08-12 15:01:30 +0500

    done

---
+++ src/Components/Func/funck.jsx
@@ -0,0 +1,154 @@
+import React, { useState } from 'react'
+import "./stayle.css"
+import Imgnav from "../../assest/wba.png"
+import Men from "../../assest/men.jpg"
+import { FcHome, } from "react-icons/fc";
+import { FaCss3, FaHtml5, FaJs, FaReact } from "react-icons/fa";
+import { GrAppsRounded } from "react-icons/gr";
+
+export const SideMene = (props) => {
+    const [inactive, setInactive] = useState(false);
+    const [open, setOpen] = useState(false)
+    const [open2, setOpen2] = useState(false)
+    const [open3, setOpen3] = useState(false)
+
+    const OpenFunction = () => {
+        setOpen(!open)
+    }
+
+
+    const OpenFunction2 = () => {
+        setOpen2(!open2)
+    }
+    const OpenFunction3 = () => {
+        setOpen3(!open3)
+    }
+
+
+    return (
+        <div className={`side_menu  ${inactive ? "inactive" : ""}`}>
+            <div className='top_section' >
+                <div className='logo' >
+                    <span className='React_i'>  <FaReact /></span>
+                    <div onClick={() => setInactive(!inactive)} className='toggele-menu-btn' >
+                        {inactive ? <i class="bi bi-arrow-right-square-fill"></i> : <i class="bi bi-arrow-left-square-fill"></i>}
+                    </div>
+                </div>
+            </div>
+            < div className='search-controller' >
+                <button className='search-btn'>
+                    <i class="bi bi-search"></i>
+                </button>
+                <input type="text" placeholder=' search me' />
+            </div>
+
+            < div className='divider' ></div>
+
+            <div className='main-menu'>
+                <ul>
+                    <li>
+                        <a className='menu-item' onClick={() => OpenFunction()}>
+                            <div className='menu-icon'>
+                                <i><GrAppsRounded /></i>
+                            </div>
+                            <span>Fontend</span>
+                        </a>
+                        {open ?
+                            <ul className={`sub-menu ${inactive ? "addColor" : ""} `} >
+                                <li>
+                                    <span><FaHtml5 /> </span>
+                                    <span>Html5</span>
+                                </li>
+                                <li>
+                                    <span><FaCss3 /> </span>
+                                    <span>Css</span>
+                                </li>
+                                <li>
+                                    <span><FaJs /> </span>
+                                    <span>Java script</span>
+                                </li>
+                            </ul>
+
+                            : ""
+                        }
+                    </li>
+                </ul>
+            </div>
+
+            <div className='main-menu'>
+                <ul>
+                    <li>
+                        <a className='menu-item' onClick={() => OpenFunction2()}>
+                            <div className='menu-icon'>
+                                <i><GrAppsRounded /></i>
+                            </div>
+                            <span>Becend</span>
+                        </a>
+                        {open2 ?
+                            <ul className={`sub-menu ${inactive ? "addColor" : ""} `} >
+                                <li>
+                                    <span><FaHtml5 /> </span>
+                                    <span>Html5</span>
+                                </li>
+                                <li>
+                                    <span><FaCss3 /> </span>
+                                    <span>Css</span>
+                                </li>
+                                <li>
+                                    <span><FaJs /> </span>
+                                    <span>Java script</span>
+                                </li>
+                            </ul>
+
+                            : ""
+                        }
+                    </li>
+                </ul>
+            </div>
+            <div className='main-menu'>
+                <ul>
+                    <li>
+                        <a className='menu-item' onClick={() => OpenFunction3()}>
+                            <div className='menu-icon'>
+                                <i><GrAppsRounded /></i>
+                            </div>
+                            <span>Android</span>
+                        </a>
+                        {open3 ?
+                            <ul className={`sub-menu ${inactive ? "addColor" : ""} `} >
+                                <li>
+                                    <span><FaHtml5 /> </span>
+                                    <span>Html5</span>
+                                </li>
+                                <li>
+                                    <span><FaCss3 /> </span>
+                                    <span>Css</span>
+                                </li>
+                                <li>
+                                    <span><FaJs /> </span>
+                                    <span>Java script</span>
+                                </li>
+                            </ul>
+
+                            : ""
+                        }
+                    </li>
+                </ul>
+            </div>
+
+
+
+            <div className='side-menu-footer'>
+                <div className='avatar'>
+                    <img src={Men} alt="" />
+                </div>
+                <div className='user-info'>
+                    <h4>Khasan</h4>
+                    <p>xasan_100@icloud.com</p>
+                    <h1></h1>
+
+                </div>
+            </div>
+        </div>
+    )
+}
